92605af9a80f5b0685f8b80dc776da20c1854bb7
楚苓 <97533612152@qq.com>
2019-02-19 21:00:55 +0800

    页面导入

--- Code/public/common/js/util.js
+++
@@ -1,7 +0,0 @@
-document.write("<script language=javascript src='/Code/public/static/jquery.js'></script>");
-function interposition() {
-    //引入导航栏bar.html
-    $(".headerpage").load("../common/html/bar.html");
-    //引入脚部的固定部分
-    $(".footerpage").load("../common/html/footer.html");
-}
