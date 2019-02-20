ab94198f935cd0bb5e575d67c7ffc340024ecdee
matthew <matthewvictorialeung@outlook.com>
2019-02-20 15:46:30 +0800

    增加js util弹出框按钮类型

--- Code/public/common/js/util.js
+++ Code/public/common/js/util.js
@@ -1,12 +1,16 @@
 open = function (url, title) {
+
+}
+openBtn = function (url, title) {
     layer.open({
         type: 1,
         title: title,
         maxmin: true,
         area: ['800px', '500px'],
-        content: url,
-        end: function () {
-            layer.tips('Hi', '#about', {tips: 1})
+        content: [url],
+        yes: function (index, layero) {
+            //do something
+            layer.close(index); //如果设定了yes回调，需进行手工关闭
         }
     });
 }
