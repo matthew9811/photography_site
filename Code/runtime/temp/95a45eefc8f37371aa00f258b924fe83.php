892a3c9b735b1d0dec9379159ee0c768a47327fa
楚苓 <97533612152@qq.com>
2019-03-02 21:21:32 +0800

    测试用户huhu

---
+++ Code/runtime/temp/95a45eefc8f37371aa00f258b924fe83.php
@@ -0,0 +1,64 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:81:"F:\photography_site\Code\public/../application/index\view\compile\post_forum.html";i:1551424080;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PhotoHome</title>
+    <?php include("/common/html/bar.html"); ?>
+    <link href="/common/css/post_forum.css" rel="stylesheet" />
+    <script src="/common/js/wangEditor.js"></script>
+</head>
+<body class="post_bg">
+<div class="container">
+    <div class="post_left">
+        <div class="post_up">
+            <div class="post_name container">The New</div>
+            <a href="/index/Forum/toInvitation" style="color: #0f0f0f;text-decoration: none">
+                <div class="post_forum container">The forum title</div>
+            </a>
+            <a href="/index/Forum/toInvitation" style="color: #0f0f0f;text-decoration: none">
+                <div class="post_forum container">The forum title</div>
+            </a>
+            <a href="/index/Forum/toInvitation" style="color: #0f0f0f;text-decoration: none">
+                <div class="post_forum container">The forum title</div>
+            </a>
+        </div>
+        <div class="post_line"></div>
+        <div class="post_up">
+            <div class="post_name container">The hot topic</div>
+            <a href="/index/Forum/toInvitation" style="color: #0f0f0f;text-decoration: none">
+                <div class="post_forum container">The forum title</div>
+            </a>
+            <a href="/index/Forum/toInvitation" style="color: #0f0f0f;text-decoration: none">
+                <div class="post_forum container">The forum title</div>
+            </a>
+            <a href="/index/Forum/toInvitation" style="color: #0f0f0f;text-decoration: none">
+                <div class="post_forum container">The forum title</div>
+            </a>
+        </div>
+    </div>
+
+    <div class="post_editor">
+        <div class="post_input_title container">
+            <div class="post_title">Post Title</div>
+            <div class="post_input">
+                <input name="title" type="text" class="form-control" placeholder="输入标题">
+            </div>
+        </div>
+        <div id="div1" class="post_wangEditor editor_toolbar"></div>
+        <div class="post_replenish">
+            <div class="post_button">
+                <input type="button" value="publish" id="btn">
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+<script type="text/javascript">
+    $(function () {
+        make_editor("#div1","#btn","/index/Forum/toPost");
+    })
+</script>
+<?php include("/common/html/footer.html"); ?>
+</html>
