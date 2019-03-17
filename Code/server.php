d3c9d4b4aed050e159cd1ed54857aa3dd866ff2c
matthew <matthewvictorialeung@outlook.com>
2019-03-17 18:55:04 +0800

    实现聊天功能后台

--- Code/server.php
+++ Code/server.php
@@ -5,3 +5,9 @@
  * Date: 2019/3/17
  * Time: 18:23
  */
+#!/usr/bin/env php
+define('APP_PATH', __DIR__ . '/application/');
+define('BIND_MODULE', 'push/Worker');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
+
